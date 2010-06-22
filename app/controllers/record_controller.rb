7884c6b7973f9e8b161a9ad542171bcdc61fda04
Steven Bedrick <steve@bedrick.org>
2010-06-21 22:26:07 -0700

    Inc. associations.

--- app/controllers/record_controller.rb
+++ app/controllers/record_controller.rb
@@ -1,7 +1,7 @@
 class RecordController < ApplicationController
   
   def show
-    @r = Record.find(params[:id])
+    @r = Record.find(params[:id], :include => [:mesh_terms, {:article => {:assigned_mesh_terms => :mesh_term}}])
   end
   
 end
