09400bb15476d1b2a68c55b5f974ab069409baff
Steven Bedrick <steve@bedrick.org>
2010-06-21 22:25:07 -0700

    New indicies to include mesh terms.

---
+++ db/migrate/20100622034534_addl_record_idx.rb
@@ -0,0 +1,24 @@
+class AddlRecordIdx < ActiveRecord::Migration
+  def self.up
+
+    # caption + title
+    execute "CREATE INDEX record_caption_title_idx ON records USING gin(to_tsvector('english', caption || title));"
+    ["caption || pubmed_mh",
+    "caption || pubmed_mh_major",
+    "caption || metamap_mh",
+    "caption || pubmed_mh || metamap_mh",
+    "caption || pubmed_mh_major || metamap_mh",
+    "caption || title || pubmed_mh",
+    "caption || title || pubmed_mh_major",
+    "caption || title || metamap_mh",
+    "caption || title || pubmed_mh || metamap_mh",
+    "caption || title || pubmed_mh_major || metamap_mh"].each do |c|
+      execute "CREATE INDEX record_#{c.split('||').map(&:strip).join('_')}_idx ON records USING gin(to_tsvector('english', #{c}));"
+    end    
+
+
+  end
+
+  def self.down
+  end
+end
